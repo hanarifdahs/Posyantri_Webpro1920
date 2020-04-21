07f8a5f9eeb31a3f855f99f240c889d608a8f232
Arctonyx <felixsonic2009@gmail.com>
2020-04-22 00:08:34 +0700

    Create kader_model.php

---
+++ application/models/kader_model.php
@@ -0,0 +1,33 @@
+<?php 
+class kader extends CI_Model{
+    function view_all() {
+        $this->db->order_by('id');
+        return $this->db->get('jadwal');
+    }
+    function insert_jadwal(){
+        $data = [
+			"nama_pasien" => $this->input->post('nama_pasien', true),
+			"nama_kader" => $this->input->post('nama_kader', true),
+			"jam" => $this->input->post('jam', true),
+		];
+        return $this->db->insert('jadwal', $data);
+    }
+    function update_jadwal(){
+        $data = [
+			"nama_pasien" => $this->input->post('nama_pasien', true),
+			"nama_kader" => $this->input->post('nama_kader', true),
+			"jam" => $this->input->post('jam', true),
+		];
+		$this->db->where('id', $id);
+		//use query builder class to update data mahasiswa based on id
+		return $this->db->update('jadwal', $data);
+    }
+    function delete_jadwal(){
+        $this->db->where('id', $id);
+		return $this->db->delete('jadwal');
+    }
+    function login(){
+        
+    }
+}
+?>
