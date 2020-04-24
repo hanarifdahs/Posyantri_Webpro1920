ca9845d0da0d9c81657dcfcad3bdffd06641f0b0
Hana Rifdah S <hanarifdahs@gmail.com>
2020-04-24 13:31:34 +0700

    update

--- application/views/pasien/header.php
+++ application/views/pasien/header.php
@@ -1,7 +1,59 @@
-<html>
-<head>
-<body>
-    <h1>INI PASIEN/HEADER.php</h1>
-    <h2>Isinya menu info tentang posyandu(home), jadwal, booking</h2>
-    <a href='<?php echo base_url('Pasien'); ?>'>Home</a>&nbsp;<a href='<?php echo base_url('Pasien/showJadwalPage'); ?>'>Jadwal</a>
-    <h4>--------------</h1>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title><?php echo SITE_NAME .": ". ucfirst($this->uri->segment(1)) ." - ". ucfirst($this->uri->segment(2)) ?></title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?=base_url()?>assets/css/carousel.css" rel="stylesheet">
+    <link href="<?=base_url()?>assets/css/style.css" rel="stylesheet">
+    <style>
+      .modal-backdrop {
+        z-index: 1;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">
+        <img src="<?=base_url()?>assets/img/logofixfix-removebg-preview-removebg-preview.png" width='50' height='50'alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="<?php echo base_url('Pasien'); ?>">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url('Pasien/showJadwalPage'); ?>">Jadwal</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Booking</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-footer">
+                            <button class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </nav>
+    </header>
