a37647bb7bf6ae14bbbdc3c1b636c055fe235517
Hana Rifdah S <hanarifdahs@gmail.com>
2020-04-24 13:31:27 +0700

    update

--- application/views/pasien/footer.php
+++ application/views/pasien/footer.php
@@ -1,5 +1,13 @@
-    <h4>______________</h4>
-    <h1>INI PASIEN/FOOTER.php</h1>
-    <h2>Isinya ya...kayak footer pada umumnya, warna aja juga boleh</h2>
+<footer class="site-footer">
+  <div id="footer-content">
+    <p>Posyantri - Webpro 19/20 </p>
+  </div>
+</footer>
+<script src="<?=base_url()?>assets/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="<?=base_url()?>assets/js/vendor/popper.min.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="<?=base_url()?>assets/js/holder.min.js"></script>
 </body>
 </html>
