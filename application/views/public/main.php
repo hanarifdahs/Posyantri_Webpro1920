6d0088f29c833871f5e342a6e55213b991439f62
Hana Rifdah S <hanarifdahs@gmail.com>
2020-04-24 12:28:46 +0700

    update isi

--- application/views/public/main.php
+++ application/views/public/main.php
@@ -1,28 +1,4 @@
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
 
-    <title><?php echo SITE_NAME .": ". ucfirst($this->uri->segment(1)) ." - ". ucfirst($this->uri->segment(2)) ?></title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="<?=base_url()?>assets/css/carousel.css" rel="stylesheet">
-    <style>
-      .modal-backdrop {
-        z-index: 1;
-      }
-    </style>
-
-</head>
-<body>
 <main role="main">
 
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -46,8 +22,7 @@
       <img class="second-slide" src="<?=base_url()?>assets/img/posyandu2.jpg" alt="Second slide">
       <div class="container">
         <div class="carousel-caption">
-          <h1>Another example headline.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <h1>Vaksin</h1>
           <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
         </div>
       </div>
@@ -56,8 +31,7 @@
       <img class="third-slide" src="<?=base_url()?>assets/img/posyandu3.jpg" alt="Third slide">
       <div class="container">
         <div class="carousel-caption text-right">
-          <h1>One more for good measure.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <h1>Senam Hamil</h1>
           <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
         </div>
       </div>
@@ -66,8 +40,7 @@
       <img class="third-slide" src="<?=base_url()?>assets/img/posyandu6.jpg" alt="Third slide">
       <div class="container">
         <div class="carousel-caption text-right">
-          <h1>One more for good measure.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <h1>Pembinaan Kader Posyandu</h1>
           <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
         </div>
       </div>
@@ -86,15 +59,15 @@
 
   <!-- START THE FEATURETTES -->
 
-  <hr class="featurette-divider">
+  <hr class="featurette-divider" >
 
   <div class="row featurette">
     <div class="col-md-7">
-      <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      <h2 class="featurette-heading">Pemeriksaan bayi usia tiga tahun</h2>
+      <p class="lead">Pemeriksaan bayi usia tiga tahun dibantu oleh kader posyandu dan diadakan pemberian vaksin untuk bayi</p>
     </div>
     <div class="col-md-5">
-      <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+      <img class="featurette-image img-fluid mx-auto" src="<?=base_url()?>assets/img/posyandu4.jpg" alt="Generic placeholder image">
     </div>
   </div>
 
@@ -102,34 +75,21 @@
 
   <div class="row featurette">
     <div class="col-md-7 order-md-2">
-      <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-    </div>
-    <div class="col-md-5 order-md-1">
-      <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-    </div>
-  </div>
-
-  <hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7">
-      <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      <h2 class="featurette-heading">Sosialisasi ibu hamil</h2>
+      <p class="lead">Pemeriksaan kesehatan bayi dalam kandungan pada ibu hamil serta sosialisasi mengenai kesehatan kandungan pada ibu hamil</p>
     </div>
     <div class="col-md-5">
-      <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+      <img class="featurette-image img-fluid mx-auto" src="<?=base_url()?>assets/img/posyandu5.jpg" alt="Generic placeholder image">
     </div>
   </div>
-
-  <hr class="featurette-divider">
 
   <!-- /END THE FEATURETTES -->
 </main>
-    <h1>Ini website</h1>
-    <a href="<?php echo base_url()?>Main/showLogin"><button>Login</button></a>
-    <?php if ($this->session->flashdata('flash')) : ?>
-    <?= $this->session->flashdata('flash'); ?>
-    <?php endif; ?>
+    <script src="<?=base_url()?>assets/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="<?=base_url()?>assets/js/vendor/popper.min.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="<?=base_url()?>assets/js/holder.min.js"></script>
 </body>
 </html>
